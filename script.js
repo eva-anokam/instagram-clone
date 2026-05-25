7acf236f348b2ca1e16508e66dd7d5d3cc08b08f
eva-anokam-aallc <eva@adebayoadeleke.com>
2026-05-25 06:29:49 +0100

    Final push

--- script.js
+++ script.js
@@ -27,3 +27,41 @@ const posts = [
         likes: 152
     }
 ]
+
+const postContainer = document.querySelector(".post-container")
+
+posts.forEach(eachPost => { 
+    const post = document.createElement("div")
+    post.classList.add("post", "flex-vertical")
+    post.innerHTML =
+    `
+        <div class="post-header flex-horizontal ">
+            <div class="post-header__img ">
+                <img src=${eachPost.avatar} alt="" class="round-img">
+            </div>
+            <div class="post-header__info">
+                <p class="post-header__name bold-text">${eachPost.name}</p>
+                <p class="post-header__location">${eachPost.location}}</p>
+            </div>
+        </div>
+
+            <div class="post-image">
+                <img src=${eachPost.post} alt="" class="post-img">
+            </div>
+
+            <div class="post-reactions flex-vertical">
+                <div class="flex-horizontal reactions">
+                    <img src="images/icon-heart.png" alt="">
+                    <img src="images/icon-comment.png" alt="">
+                    <img src="images/icon-dm.png" alt="">
+                </div>
+                <p class="bold-text">${eachPost.likes} likes</p>
+                <div class="post-content">
+                    <p><span class="bold-text">${eachPost.username}</span> ${eachPost.comment}</p>
+                </div>
+            </div>
+
+
+    `
+    postContainer.appendChild(post)
+})
